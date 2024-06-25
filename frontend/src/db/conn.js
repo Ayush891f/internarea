1b41badb17611393037016932758416041262523
Ayush891f <127117290+Ayush891f@users.noreply.github.com>
2024-06-25 16:58:10 +0530

    files added

--- frontend/src/db/conn.js
+++ frontend/src/db/conn.js
@@ -1,9 +1,9 @@
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: '.env' });
 
-const db="mongodb+srv://ayushgupta68687:<ayushgupta>@cluster0.dckalvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const db="process.env.DATABASE"
 const local="mongodb://127.0.0.1:27017"
 mongoose.connect(db, {
     useNewUrlParser: true,
